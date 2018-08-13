83dc7b022ee0c6cc962d43f275b07439836c58be
Yanuar Arifin <3326885+nangnual@users.noreply.github.com>
2018-08-13 14:59:47 +0700

    add SQLAlchemy and make table user & post with their migrations

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = 'shhs-shhhhs-sshhhsh'
+    SECRET_KEY = 'shhs-shhhhs-sshhhsh'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATION = False
